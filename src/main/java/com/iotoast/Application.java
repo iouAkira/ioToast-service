9b6a3f0b2f5e5152652ec1f72cc0ee041e484860
Akira <e.akimoto.akira@gmail.com>
2019-08-02 10:53:54 +0800

    Dev (#3)
    * init (#3)

--- src/main/java/com/iotoast/Application.java
+++ src/main/java/com/iotoast/Application.java
@@ -1,7 +1,9 @@
 package com.iotoast;
 
+import com.iotoast.utils.Resource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
 public class Application {
